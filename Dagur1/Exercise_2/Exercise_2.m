db6774643e79e6d4091712692d3c5770a9c37c8f
ragnheidurgna <ragnheidurg23@ru.is>
2024-04-29 14:17:51 +0000

    allt ready í 2

--- Dagur1/Exercise_2/Exercise_2.m
+++ Dagur1/Exercise_2/Exercise_2.m
@@ -1,6 +1,6 @@
 clear all; close all; clc;
 
-% Define vertices of a square
+% Define vertices
 Vertices = [1 1;7 2;6 -4;3 -2;-5 -6;-2 0;-4 10;2 8];
 numRotations = 10;
 angleDeg = 5;
@@ -9,22 +9,20 @@ rotatePolygon(Vertices, numRotations, angleDeg);
 
 function rotatePolygon(vertices, numRotations, angleDeg)
     center = [0, 0];
-
     %Closing the path
     vertices(end+1, :) = vertices(1, :);
 
-    % Create a figure
+    %Plotting otiginal polygon and center
     figure;
     hold on;
     axis equal;
     title('Rotating Polygon');
     axis([-12 12 -12 12]);
+    plot(0, 0, 'ro', 'MarkerFaceColor', 'r');
+    polygonPlot = plot(vertices(:,1), vertices(:,2), 'b-', 'LineWidth', 2);
     
     % Convert the angle from degrees to radians
     angleRad = deg2rad(angleDeg);
-    plot(0, 0, 'ro', 'MarkerFaceColor', 'r');
-    polygonPlot = plot(vertices(:,1), vertices(:,2), 'b-', 'LineWidth', 2);
-
     for k = 1:numRotations
         %Rotation matrix
         R = [cos(angleRad) -sin(angleRad); sin(angleRad) cos(angleRad)];
