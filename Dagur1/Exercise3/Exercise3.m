0b7883339d1452dda1d50c4f37f0b8a59d00c04f
ragnheidurgna <ragnheidurg23@ru.is>
2024-04-29 11:53:15 +0000

    Setti þetta í function

--- Dagur1/Exercise3/Exercise3.m
+++ Dagur1/Exercise3/Exercise3.m
@@ -1,25 +1,26 @@
 clear all
 %%
-%create grid
-N = 20;
-[x,y] = meshgrid(1:N, 1:N); %Create grid
 
-P = 0.2; %Obstacle density
-obstacles = rand(N) > 1-P; 
-%%
-%VISUALIZE GRID
-% plot(x,y,'b.') 
+obstaclePathfinder(20,0.2);
 
-%VISUALIZE OBSTACLES
-plot(x(obstacles == 1),y(obstacles == 1),'ro','MarkerSize',7.5)
+function obstaclePathfinder(N,P)
+    [x,y] = meshgrid(1:N, 1:N); %Create grid
 
-% sum(obstacles(:))/(N*N) % real obstacle density
-%%
-y_now = 1;%
-x_now = 1;% starting point upper right
-path = [x_now y_now];
-skref = 0
-while y_now ~= N
+    obstacles = rand(N) > 1-P; 
+    %%
+    %VISUALIZE GRID
+    % plot(x,y,'b.') 
+    
+    %VISUALIZE OBSTACLES
+    plot(x(obstacles == 1),y(obstacles == 1),'ro','MarkerSize',7.5)
+    
+    % sum(obstacles(:))/(N*N) % real obstacle density
+    %%
+    y_now = 1;%
+    x_now = 1;% starting point upper right
+    path = [x_now y_now];
+    skref = 0
+    while y_now ~= N
     if obstacles(y_now,x_now) == 1
         x_now = x_now+1; 
         continue
@@ -35,9 +36,10 @@ while y_now ~= N
     end
     path = [path;x_now y_now];
     skref = skref+1
+    end
+    
+    hold on               %N+1-y flipping the y axis to align with matrix indeces
+    plot(x(obstacles == 1),N+1-y(obstacles == 1),'ro','MarkerSize',7.5)
+    plot(starting_point(1),starting_point(2), 'blacko','MarkerFaceColor','b','MarkerSize',10)
+    hold off
 end
-
-hold on               %N+1-y flipping the y axis to align with matrix indeces
-plot(x(obstacles == 1),N+1-y(obstacles == 1),'ro','MarkerSize',7.5)
-plot(starting_point(1),starting_point(2), 'blacko','MarkerFaceColor','b','MarkerSize',10)
-hold off
