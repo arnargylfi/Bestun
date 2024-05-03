6e382e432b299d0e3531f818e85de157074e1b9c
Arnar Gylfi <arnargylfi01@gmail.com>
2024-05-03 12:02:23 +0000

    Exercise 1 komið

--- Dagur4/Exercise1.m
+++ Dagur4/Exercise1.m
@@ -1,12 +1,14 @@
+format short
 eff1 = @(x) (x-2).^2;
 eff2 = @(x) x.^2+3*exp(-2*x);
 tol = 1e-5;
 x1 = 0;
 x2 = 10;
-
+figure()
 subplot(1,2,1)
-title('$(x-2)^2$','Interpreter','latex')
-goldensearch(eff1,x1,x2,tol);
+[iter1,func_min1,x_min1] = goldensearch(eff1,x1,x2,tol);
+title(sprintf('$f(x) = (x-2)^2$, $(x,f(x))_{\\mathrm{min}} = (%f, %f)$, number of iterations $= %d$', x_min1, func_min1, iter1),'Interpreter','latex','FontSize',12)
 subplot(1,2,2)
-title('$x^2+3e^{-2x}$','Interpreter','latex')
-goldensearch(eff2,x1,x2,tol);
+[iter2,func_min2,x_min2] = goldensearch(eff2,x1,x2,tol);
+title(sprintf('$f(x) = x^2+3e^{-2x}$, $(x,f(x))_{\\mathrm{min}} = (%f, %f)$, number of iterations $= %d$', x_min2, func_min2, iter2),'Interpreter','latex','FontSize',12)
+sgtitle('Golden Ratio search for minima of two functions')
--- Dagur4/goldensearch.m
+++ Dagur4/goldensearch.m
@@ -1,4 +1,4 @@
-function [func_min, x_min] = goldensearch(func,x1,x2,tolerance)
+function [iter, func_min, x_min] = goldensearch(func,x1,x2,tolerance)
 goldenratio = (1+sqrt(5))/2;
 X = [x1,x2,0,0];
 [func_min,x_mindex] = min(func(X));
@@ -7,21 +7,24 @@ sprintf('Minimal function value = %f, value of x = %f',func_min,X(x_mindex))
 plot(x1:abs(x1-x2)/100:x2,func(x1:abs(x1-x2)/100:x2))
 hold on
 c = linspace(1,10,length(X));
+scatter_handle = scatter(X, func(X),[],c,'filled');
+iter = 0;
 while abs(X(2)-X(1)) > tolerance
     X(3) = X(2)-(X(2)-X(1))/goldenratio;
     X(4) = X(1)+(X(2)-X(1))/goldenratio;
+    set(scatter_handle, 'Visible', 'off') % Clear scatter plot points
     scatter_handle = scatter(X, func(X),[],c,'filled');
+    pause(2*abs((X(2)-X(1))/(x1-x2)))
     [func_min,x_mindex] = min(func(X));
     x_min = X(x_mindex);
     sprintf('Minimal function value = %f, value of x = %f',func_min,x_min)
-    pause(0.2)
     [~,max_index] = max(func(X));
     if max_index == 1
         X(1) = X(3);
     elseif max_index == 2
         X(2) = X(4);
     end
-    set(scatter_handle, 'Visible', 'off') % Clear scatter plot points
+    iter = iter+1;
 end
 sprintf('Final minimal function value = %f, value of x = %f',func_min,x_min)
 end
