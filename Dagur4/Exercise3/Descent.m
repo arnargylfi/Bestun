5cd96bd239554900f595dafb9067ec451c7e744e
ragnheidurgna <ragnheidurg23@ru.is>
2024-05-03 13:02:18 +0000

    allt í rugli hjá mér

--- Dagur4/Exercise3/Descent.m
+++ Dagur4/Exercise3/Descent.m
@@ -1,16 +1,29 @@
 function x_min = trust_region_descent(f, x, delta, kmax, tol)
     k = 0;
     found = false;
+    h=1e-5;
+    hvec = min(x):h:max(x);
     
     while ~found && k <= kmax
         k = k + 1;
         
-        grad = finite_diff_gradient(f, x, 1e-5);
-        htr = -delta * grad / norm(grad);
+        grad = finite_diff_gradient(f, x, h);
+        if norm(grad) == 0
+            disp('Norm(grad)=0');
+            x_min = x;
+            break;
+        end
+        disp(size(hvec))
+        disp(size(grad))
+        % htr = -delta * grad / norm(grad);
+        q = f(x) + grad.*hvec;
+        disp(size(q));
+        htr = min(q);
+        disp(size(htr))
         
         q0 = f(x);
         qhtr = f(x + htr);
-        actual_reduction = q0 - f(x + htr);
+        actual_reduction = f(x) - f(x + htr);
         predicted_reduction = q0 - qhtr;
 
         if predicted_reduction == 0
@@ -44,10 +57,13 @@ function grad = finite_diff_gradient(f, x, h)
     n = length(x);
     grad = zeros(n, 1);
     for i = 1:n
-        x_forward = x;
-        x_forward(i) = x_forward(i) + h;
-        f_forward = f(x_forward);
-        f_current = f(x);
-        grad(i) = (f_forward - f_current) / h;
+        x_plus = x;
+        x_minus = x;
+        x_plus(i) = x_plus(i) + h;
+        x_minus(i) = x_minus(i) - h;
+        f_plus = f(x_plus);
+        f_minus = f(x_minus);
+        grad(i) = (f_plus - f_minus) / (2 * h);
     end
 end
+
--- Dagur4/Exercise3/Exercise3.m
+++ Dagur4/Exercise3/Exercise3.m
@@ -1,1 +1,28 @@
 %Test
+% Test functions
+f1 = @(x) sum(x.^2);          % Quadratic function
+g = @(x) sin(x);              % Sin function
+h = @(x) exp(x(1)/5 + x(2)/2) + x(1)^2 + x(2)^2; % Exponential and quadratic
+rosenbrock = @(x) (1-x(1))^2 + 100*(x(2)-x(1)^2)^2; % Rosenbrock function
+
+% Starting points
+x0_1 = (1:10)'; % For f1 with n = 10
+x0_2 = 2;       % For g
+x0_3 = [5; 3];  % For h
+x0_4 = [0; 0];  % For Rosenbrock
+
+% Call the trust region method
+x_min_1 = Descent(f1, x0_1, 1, 100, 1e-6);
+% x_min_2 = Descent(g, x0_2, 1, 100, 1e-6);
+% x_min_3 = Descent(h, x0_3, 1, 100, 1e-6);
+% x_min_4 = Descent(rosenbrock, x0_4, 1, 100, 1e-6);
+
+% Display results
+disp('Results for f1:');
+disp(x_min_1);
+disp('Results for g:');
+disp(x_min_2);
+disp('Results for h:');
+disp(x_min_3);
+disp('Results for Rosenbrock:');
+disp(x_min_4);
