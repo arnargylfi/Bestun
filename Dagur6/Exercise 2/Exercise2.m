4b93a3fd36cd03200cea6d6d2879874189db6e79
Arnar Gylfi <arnargylfi01@gmail.com>
2024-05-07 11:36:56 +0000

    Exercise 2

--- Dagur6/Exercise 2/Exercise2.m
+++ Dagur6/Exercise 2/Exercise2.m
@@ -1,12 +1,19 @@
 f = @(X) exp(sum(X./(1:length(X))));
-nonlcon = @(x) deal([], norm(x) - 1);
 
 options = optimset('Display','iter','TolX',1e-8,'TolFun',1e-8);
 
 for n = [2,3,4,20]
     fprintf('Minimizing f with fmincon, n = %d',n)
     X0 = zeros(1,n);
-    fmincon(f,X0,[],[],[],[],[],[],nonlcon,options)
+    fmincon(f,X0,[],[],[],[],[],[],@constr,options)
     pause(0.5)
 end
+
+
+function [c,ceq] = constr(X)
+    ceq = [];
+    c = norm(X)^2-1;
+end
+
+
 
