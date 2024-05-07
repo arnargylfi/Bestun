462511249d6056b5743d470382b7ef2b8affbbc7
Arnar Gylfi <arnargylfi01@gmail.com>
2024-05-07 10:50:19 +0000

    Exercise 2

--- Dagur6/Exercise1/Exercise1.m
+++ Dagur6/Exercise1/Exercise1.m
@@ -1,7 +1,7 @@
 f = @(X) sum((1-X(1:end-1)).^2+100.*(X(2:end)-X(1:end-1).^2).^2);
 options = optimset('Display','iter','TolX',1e-8,'TolFun',1e-8,'MaxFunEvals',100000,'MaxIter',100000);
 for n = [3,5,10]
-    display(sprintf('Minimizing Rosenbrock function with fminunc, n = %d',n))
+    fprintf('Minimizing Rosenbrock function with fminunc, n = %d',n)
     X0 = zeros(1,n);
     fminunc(f,X0,options)
     pause(0.5)
@@ -10,8 +10,9 @@ end
 %%
 
 for n = [3,5,10]
-    display(sprintf('Minimizing Rosenbrock function with fminsearch, n = %d',n))
+    fprintf('Minimizing Rosenbrock function with fminsearch, n = %d',n)
     X0 = zeros(1,n);
     fminsearch(f,X0,options)
     pause(0.5)
 end
+
