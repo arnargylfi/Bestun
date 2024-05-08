8cba9c090da717912e7273d95aebf465854366d4
HafthorArni <111596842+HafthorArni@users.noreply.github.com>
2024-05-08 14:42:11 +0000

    dfv

--- Dagur7/Exercise2/exercise2.m
+++ Dagur7/Exercise2/exercise2.m
@@ -17,26 +17,42 @@ n = 2; % Dimensionality of the problem
 kmax = 1000; % Number of iterations
 
 % Execute random search
-[xbest, fbest, history, all_points, all_values] = smarterRandomSearch(fr, n, fr_bounds, kmax);
+[xbest, fbest, history, all_points, all_values] = smartRandomSearch(fr, n, fr_bounds, kmax);
 disp('Rosenbrock')
 fbest, xbest
-%plotAnimation(fr, all_points, fr_bounds, kmax);
+plotAnimation(fr, all_points, fr_bounds, kmax);
+title('Rosenbrock')
+hold on
+plot(xbest(1),xbest(2),'kx','linewidth',2)
+hold off
 
-[xbest, fbest, history, all_points, all_values] = smarterRandomSearch(fp, n, fp_bounds, kmax);
+[xbest, fbest, history, all_points, all_values] = smartRandomSearch(fp, n, fp_bounds, kmax);
 disp('fp')
 fbest, xbest
-%plotAnimation(fp, all_points, fp_bounds, kmax);
-
-[xbest, fbest, history, all_points, all_values] = smarterRandomSearch(fa, n, fa_bounds, kmax);
+plotAnimation(fp, all_points, fp_bounds, kmax);
+title('fp')
+hold on
+plot(xbest(1),xbest(2),'kx','linewidth',2)
+hold off
+%
+[xbest, fbest, history, all_points, all_values] = smartRandomSearch(fa, n, fa_bounds, kmax);
 disp('Auckley')
 fbest, xbest
-%plotAnimation(fa, all_points, fa_bounds, kmax);
+plotAnimation(fa, all_points, fa_bounds, kmax);
+title('Auckley')
+hold on
+plot(xbest(1),xbest(2),'kx','linewidth',2)
+hold off
+
 
 %% Comparing the methods
 
-n_runs = 100;
-algorithms = {@smartRandomSearch, @smarterRandomSearch};
-algorithm_names = {'Smart Random Search', 'Smarter Random Search'};
+n_runs = 1000;
+n = 2
+%algorithms = {@smartRandomSearch, @smarterRandomSearch};
+%algorithm_names = {'Smart Random Search', 'Smarter Random Search'};
+algorithms = {@randomSearch, @smartRandomSearch};
+algorithm_names = {'Random Search', 'Smart Random Search'};
 function_handles = {fr, fp, fa};
 function_names = {'Rosenbrock', 'fp', 'Auckley'};
 function_solutions = [fr_solution, fp_solution, fa_solution];
@@ -49,7 +65,7 @@ for j = 1:length(function_handles)
     for i = 1:length(algorithms)
         [meanBest, stdBest, maxBest, minBest] = evaluateMethods(algorithms{i}, function_handles{j}, n, function_bounds(j,:), kmax, n_runs, function_solutions(j));
         stats(i, :, j) = [meanBest, stdBest, maxBest, minBest];
-        fprintf('%s: Mean: %f, Std: %f, Min: %f, Max: %f\n', algorithm_names{i}, meanBest, stdBest, minBest, maxBest);
+        fprintf('%s: Mean Error: %f, Std Error: %f, Min Error: %f, Max Error: %f\n', algorithm_names{i}, meanBest, stdBest, minBest, maxBest);
     end
 end
 
@@ -60,6 +76,6 @@ end
         title(sprintf('Absolute error Metrics for %s Function', function_names{j}));
         xlabel('Metric');
         ylabel('Value');
-        set(gca, 'XTickLabel', {'Mean', 'Std', 'Max', 'Min'});
+        set(gca, 'XTickLabel', {'Mean Error', 'Std Error', 'Max Error', 'Min Error'});
         legend(algorithm_names, 'Location', 'northoutside', 'Orientation', 'horizontal');
     end
