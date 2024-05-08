7fd19c4a7c3a49e02380243cf0959a1bf392b85d
ragnheidurgna <ragnheidurg23@ru.is>
2024-05-08 15:24:52 +0000

    bæjo

--- Dagur7/Exercise_3/Exercise3.m
+++ Dagur7/Exercise_3/Exercise3.m
@@ -2,22 +2,21 @@ clear all;
 close all;
 clc;
 
-% Define the functions, bounds, and global minimum solutions
+%Define the functions, bounds, and global minimum solutions
 functions = {@(x) sum((1 - x(1:end-1)).^2 + 100 .* (x(2:end) - x(1:end-1).^2).^2), ... % Rosenbrock
              @(x) -exp(-norm(x)^2/2) * prod(cos(10*x)), ... % Function fp
              @(x) -20 * exp(-0.2 * sqrt(mean(x.^2))) - exp(mean(cos(2*pi*x))) + 21}; % Ackley
 
-bounds = {[-2, 2], [-2, 2], [-10, 10]}; % Bounds for each function
-solutions = [0, -1, -1.7183]; % Global minimum solutions for the functions
-names = {'Rosenbrock', 'fp', 'Ackley'}; % Names of the functions
-n_values = [2, 2, 2]; % Number of variables for each function
-F = [0.5, 1, 1.5]; % F values to test
-CR = [0.1, 0.5, 0.9]; % CR values to test
-maxGens = 1000; % Maximum generations
+bounds = {[-2, 2], [-2, 2], [-10, 10]};
+solutions = [0, -1, -1.7183];
+names = {'Rosenbrock', 'fp', 'Ackley'};
+n_values = [2, 2, 2];
+F = [0.5, 1, 1.5];
+CR = [0.1, 0.5, 0.9];
+maxGens = 1000;
 
-% Run the differential evolution algorithm for each function and parameter set
-n_runs = 30; % Number of runs for averaging results
-stats = zeros(length(functions), length(F) * length(CR), 4); % To store statistics
+n_runs = 30;
+stats = zeros(length(functions), length(F) * length(CR), 4);
 
 for i = 1:length(functions)
     fun = functions{i};
@@ -52,34 +51,31 @@ for i = 1:length(functions)
 end
 
 %%
+%Testing dimensions
 Ros = functions{1};
-R_F = 0.5;
-R_CR = 0.1;
-fprintf('Evaluating n= 1 for the f_p function:\n')
+F = 0.5;
+CR = 0.1;
+fprintf('Evaluating n= 1 for the Rosenbrock function:\n')
 for l = 1:1
-    [meanBest, stdBest, maxBest, minBest] = evaluateMethods(@Diff_Evolution, fp, l, bounds{1}, maxGens, n_runs, solutions{1}, fp_F, fp_CR);
+    [meanBest, stdBest, maxBest, minBest] = evaluateMethods(@Diff_Evolution, Ros, l, [-2 2], maxGens, n_runs, 0, F, CR);
     fprintf('Stats for n = %.d: Mean = %.5f, Std = %.5f, Max = %.5f, Min = %.5f\n',l, meanBest, stdBest, maxBest, minBest);
 end
 fp = functions{2};
-fp_F = 0.5;
-fp_CR = 0.1;
 fprintf('Evaluating n= 1 & 2 for the f_p function:\n')
 for m = 1:2
-    [meanBest, stdBest, maxBest, minBest] = evaluateMethods(@Diff_Evolution, fp, m, bounds{2}, maxGens, n_runs, solutions{2}, fp_F, fp_CR);
+    [meanBest, stdBest, maxBest, minBest] = evaluateMethods(@Diff_Evolution, fp, m, [-2 2], maxGens, n_runs, -1, F, CR);
     fprintf('Stats for n = %.d: Mean = %.5f, Std = %.5f, Max = %.5f, Min = %.5f\n',m, meanBest, stdBest, maxBest, minBest);
 end
 Ack = functions{3};
-Ack_F = 0.5;
-Ack_CR = 0.1;
 fprintf('Evaluating n= 1,2 & 3 for the Ackley function:\n')
 for g = 1:3
-    [meanBest, stdBest, maxBest, minBest] = evaluateMethods(@Diff_Evolution, Ack, g, bounds{3}, maxGens, n_runs, solutions{3}, Ack_F, Ack_CR);
+    [meanBest, stdBest, maxBest, minBest] = evaluateMethods(@Diff_Evolution, Ack, g, [-10 10], maxGens, n_runs, -1.7183, F, CR);
     fprintf('Stats for n = %.d: Mean = %.5f, Std = %.5f, Max = %.5f, Min = %.5f\n',g, meanBest, stdBest, maxBest, minBest);
 end
 
 
 %%
-%Plotting for best values of F anf CR
+%Visulization of the history for each function
 [R_all_points,~,~] = Diff_Evolution(functions{1},2,bounds{1},0.5,0.1,1000);
 [F_all_points,~,~] = Diff_Evolution(functions{2},2,bounds{2},0.5,0.1,1000);
 [A_all_points,~,~] = Diff_Evolution(functions{3},2,bounds{3},0.5,0.1,1000);
